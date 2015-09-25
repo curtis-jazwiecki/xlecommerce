c878cc7a5e34b784bfb2a105880f874a1395726b
OBN Dev Team <curt@outdoorbusinessnetwork.com>
2015-09-25 22:53:20 +0000

    categories_bullet.php edited online with Bitbucket - removed ucwords(strtolower) command to remove forced lowercase of category names

--- includes/modules/categories_bullet.php
+++ includes/modules/categories_bullet.php
@@ -12,13 +12,13 @@
       $col++;
       $cPath_new = tep_get_path($categories['categories_id']);
       $width = (int)(100 / MAX_DISPLAY_CATEGORIES_PER_ROW) . '%';
-      echo '              <table cellpadding="0" cellspacing="0" width="100%"><tr><td class="cat_title"><a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new) . '" class="cat_title">' . ucwords(strtolower($categories['categories_name'])) . '</a> >></td></tr>';
+      echo '              <table cellpadding="0" cellspacing="0" width="100%"><tr><td class="cat_title"><a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new) . '" class="cat_title">' . ($categories['categories_name']) . '</a> >></td></tr>';
       $sub_cat_query = tep_db_query("select c.categories_id, cd.categories_name, c.parent_id from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd where c.parent_id = '" . (int)$categories['categories_id'] . "' and c.categories_id = cd.categories_id and cd.language_id = '" . (int)$languages_id . "' order by sort_order, cd.categories_name");
   echo '<tr><td class="cat_list">'; 
     if(tep_db_num_rows($sub_cat_query) > 0){
       while ($sub_cat = tep_db_fetch_array($sub_cat_query)) {
       		      $cPath_new_1 = "cPath="  . $current_category_id . "_" . $categories['categories_id'] . "_" . $sub_cat['categories_id'];
-      echo ' &#149 <a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new_1) . '" class="cat_list">' . ucwords(strtolower($sub_cat['categories_name'])). '</a><br>';     
+      echo ' &#149 <a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new_1) . '" class="cat_list">' . ($sub_cat['categories_name']). '</a><br>';     
           }
     } else {
       	$products_in_category = tep_count_products_in_category($categories['categories_id']);
